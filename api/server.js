185eb58fbf3a9d9a1e81667305363e5336a93284
oyewunmi gbenga kelvin <kelvinoye@gmail.com>
2025-01-04 11:31:02 +0100

    Fix: test and build run

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors"); // Import CORS
 const port = 5000;
 const server = express();
 const app = require("./home");
 const services = require("./services");
 
+
+server.use(cors());
 server.use(app);
 server.use(services);
 server.listen(port, () => {
